3bd4087d2e14c0c50427e5e53e02a75501017117
bebi <bilguun.bayarsaikhan@unimedia.mn>
2022-02-01 10:35:19 +0800

    #4 wc payment gateway

--- qplugin.php
+++ qplugin.php
@@ -15,7 +15,7 @@
  * @wordpress-plugin
  * Plugin Name:       QPlugin
  * Plugin URI:        https://github.com/biligunb/qplugin
- * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Description:       WooCommerce - Payment Gateway using QPay
  * Version:           1.0.0
  * Author:            Unimedia Solutions LLC engineers
  * Author URI:        https://unimedia.mn/
@@ -63,6 +63,142 @@ register_deactivation_hook( __FILE__, 'deactivate_qplugin' );
  * admin-specific hooks, and public-facing site hooks.
  */
 require plugin_dir_path( __FILE__ ) . 'includes/class-qplugin.php';
+
+
+// Make sure WooCommerce is active
+if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) return;
+
+/*
+ * This action hook registers our PHP class as a WooCommerce payment gateway
+ */
+add_filter( 'woocommerce_payment_gateways', 'qplugin_add_gateway_class' );
+function qplugin_add_gateway_class( $gateways ) {
+	$gateways[] = 'WC_QPlugin_Gateway';
+	return $gateways;
+}
+
+/*
+ * The class itself, please note that it is inside plugins_loaded action hook
+ */
+add_action( 'plugins_loaded', 'qplugin_init_gateway_class' );
+function qplugin_init_gateway_class() {
+	class WC_QPlugin_Gateway extends WC_Payment_Gateway {
+ 		/**
+ 		 * Class constructor
+ 		 */
+ 		public function __construct() {
+			$this->id = 'qplugin'; // payment gateway plugin ID
+			$this->icon = ''; // URL of the icon that will be displayed on checkout page near your gateway name
+			$this->has_fields = true; // in case you need a custom credit card form
+			$this->method_title = 'QPlugin Gateway';
+			$this->method_description = 'Payment using Qpay'; // will be displayed on the options page
+		
+			// gateways can support subscriptions, refunds, saved payment methods,
+			// but in this tutorial we begin with simple payments
+			$this->supports = array(
+				'products'
+			);
+		
+			// Method with all the options fields
+			$this->init_form_fields();
+		
+			// Load the settings.
+			$this->init_settings();
+			$this->title = $this->get_option( 'title' );
+			$this->description = $this->get_option( 'description' );
+			$this->enabled = $this->get_option( 'enabled' );
+			$this->testmode = 'yes' === $this->get_option( 'testmode' );
+			$this->private_key = $this->testmode ? $this->get_option( 'test_private_key' ) : $this->get_option( 'private_key' );
+			$this->publishable_key = $this->testmode ? $this->get_option( 'test_publishable_key' ) : $this->get_option( 'publishable_key' );
+		
+			// This action hook saves the settings
+			add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+		
+			// We need custom JavaScript to obtain a token
+			add_action( 'wp_enqueue_scripts', array( $this, 'payment_scripts' ) );
+			
+			// You can also register a webhook here
+			// add_action( 'woocommerce_api_{webhook name}', array( $this, 'webhook' ) );
+		}
+
+		/**
+ 		 * Plugin options
+ 		 */
+ 		public function init_form_fields(){
+			$this->form_fields = array(
+				'enabled' => array(
+					'title'       => 'Enable/Disable',
+					'label'       => 'Enable QPlugin Gateway',
+					'type'        => 'checkbox',
+					'description' => '',
+					'default'     => 'no'
+				),
+				'title' => array(
+					'title'       => 'Title',
+					'type'        => 'text',
+					'description' => 'This controls the title which the user sees during checkout.',
+					'default'     => 'Credit Card',
+					'desc_tip'    => true,
+				),
+				'description' => array(
+					'title'       => 'Description',
+					'type'        => 'textarea',
+					'description' => 'This controls the description which the user sees during checkout.',
+					'default'     => 'Pay with your credit card via our super-cool payment gateway.',
+				),
+				'testmode' => array(
+					'title'       => 'Test mode',
+					'label'       => 'Enable Test Mode',
+					'type'        => 'checkbox',
+					'description' => 'Place the payment gateway in test mode using test API keys.',
+					'default'     => 'yes',
+					'desc_tip'    => true,
+				),
+				'test_publishable_key' => array(
+					'title'       => 'Test Publishable Key',
+					'type'        => 'text'
+				),
+				'test_private_key' => array(
+					'title'       => 'Test Private Key',
+					'type'        => 'password',
+				),
+				'publishable_key' => array(
+					'title'       => 'Live Publishable Key',
+					'type'        => 'text'
+				),
+				'private_key' => array(
+					'title'       => 'Live Private Key',
+					'type'        => 'password'
+				)
+			);
+		}
+
+		/**
+		 * You will need it if you want your custom credit card form
+		 */
+		public function payment_fields() { }
+
+		/*
+		 * Custom CSS and JS, in most cases required only when you decided to go with a custom credit card form
+		 */
+	 	public function payment_scripts() { }
+
+		/*
+ 		 * Fields validation
+		 */
+		public function validate_fields() { }
+
+		/*
+		 * We're processing the payments here
+		 */
+		public function process_payment( $order_id ) { }
+
+		/*
+		 * In case you need a webhook, like PayPal IPN etc
+		 */
+		public function webhook() { }
+ 	}
+}
 
 /**
  * Begins execution of the plugin.
